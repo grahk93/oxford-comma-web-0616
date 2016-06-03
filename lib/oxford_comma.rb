d8f6d9b3fc895366eb79917467a2ac56c56a3c13
grahk93 <grace.juster@flatironschool.com>
2016-06-03 13:29:56 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+   last = array.pop
+   array_string = array.join(", ")
+   array_string << ", and #{last}"
+  end
 end
